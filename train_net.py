781ef325db01e4c72b38ba1d2db4155a0bc8e07d
amajee11us <anay.majee@utdallas.edu>
2025-02-05 13:42:59 -0600

    Added modified configs and resume strategy for training

--- train_net.py
+++ train_net.py
@@ -89,6 +89,28 @@ class Trainer(DefaultTrainer):
 
         self.register_hooks(self.build_hooks())
 
+    def resume_or_load(self, resume=True):
+        """
+        If `resume==True` and `cfg.OUTPUT_DIR` contains the last checkpoint (defined by
+        a `last_checkpoint` file), resume from the file. Resuming means loading all
+        available states (eg. optimizer and scheduler) and update iteration counter
+        from the checkpoint. ``cfg.MODEL.WEIGHTS`` will not be used.
+
+        Otherwise, this is considered as an independent training. The method will load model
+        weights from the file `cfg.MODEL.WEIGHTS` (but will not load other states) and start
+        from iteration 0.
+
+        Args:
+            resume (bool): whether to do resume or not
+        """
+        print("=== Overridden resume_or_load in Trainer! ===")
+        # Do not resume the checkpoint but just load the weights
+        self.checkpointer.resume_or_load(self.cfg.MODEL.WEIGHTS, resume=False) 
+        if resume and self.checkpointer.has_checkpoint():
+            # The checkpoint stores the training iteration that just finished, thus we start
+            # at the next iteration
+            self.start_iter = self.iter + 1
+
     @classmethod
     def build_model(cls, cfg):
         """
