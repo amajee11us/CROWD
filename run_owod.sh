6bfedd12aa5dd57a262de3cddfb1e95b9da757c1
amajee11us <anay.majee@utdallas.edu>
2025-05-12 14:20:27 -0500

    Do not mine unknowns for T4 - no unknowns left

--- run_owod.sh
+++ run_owod.sh
@@ -5,7 +5,7 @@ BENCHMARK=${BENCHMARK:-"M-OWODB"}  # M-OWODB or S-OWODB
 PORT=${PORT:-"50211"}
 GPUS=${GPUS:-"0"}
 NUM_GPUS=${NUM_GPUS:-"1"}
-BASELINE=${BASELINE:-"baseline_OD_sel"}
+BASELINE=${BASELINE:-"crowd_l_gccg_d_gccg"}
 
 if [ $BENCHMARK == "M-OWODB" ]; then
   # T1
@@ -94,12 +94,12 @@ if [ $BENCHMARK == "M-OWODB" ]; then
                                 MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t3/model_0029999.pth \
                                 OUTPUT_DIR output/${BASELINE}/${BENCHMARK}/t4/
   
-  CUDA_VISIBLE_DEVICES=${GPUS} python discover_unknown.py \
-                                --config-file configs/${BENCHMARK}/t4_ft.yaml \
-                                --input-txt datasets/ImageSets/Main/${BENCHMARK}/t4_ft.txt \
-                                --task ${BENCHMARK}/t4_ft \
-                                --output output/${BASELINE}/${BENCHMARK}/t4/unknown_rois.json \
-                                --opts MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t4/model_0014999.pth DISCOVER_UNKNOWN True 
+  # CUDA_VISIBLE_DEVICES=${GPUS} python discover_unknown.py \
+  #                               --config-file configs/${BENCHMARK}/t4_ft.yaml \
+  #                               --input-txt datasets/ImageSets/Main/${BENCHMARK}/t4_ft.txt \
+  #                               --task ${BENCHMARK}/t4_ft \
+  #                               --output output/${BASELINE}/${BENCHMARK}/t4/unknown_rois.json \
+  #                               --opts MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t4/model_0014999.pth DISCOVER_UNKNOWN True 
 
   CUDA_VISIBLE_DEVICES=${GPUS} python train_net.py \
                                 --num-gpus ${NUM_GPUS} \
@@ -108,7 +108,6 @@ if [ $BENCHMARK == "M-OWODB" ]; then
                                 --config-file configs/${BENCHMARK}/t4_ft.yaml \
                                 --resume \
                                 MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t4/model_0014999.pth \
-                                DISCOVER_STORE_PATH output/${BASELINE}/${BENCHMARK}/t4/unknown_rois.json \
                                 OUTPUT_DIR output/${BASELINE}/${BENCHMARK}/t4/
 else
   # T1
@@ -197,12 +196,12 @@ else
                                 MODEL.WEIGHTS output/${BENCHMARK}/t3/model_0029999.pth \
                                 OUTPUT_DIR output/${BASELINE}/${BENCHMARK}/t4/
   
-  CUDA_VISIBLE_DEVICES=${GPUS} python discover_unknown.py \
-                                --config-file configs/${BENCHMARK}/t4_ft.yaml \
-                                --input-txt datasets/ImageSets/Main/${BENCHMARK}/t4_ft.txt \
-                                --task ${BENCHMARK}/t4_ft \
-                                --output output/${BASELINE}/${BENCHMARK}/t4/unknown_rois.json \
-                                --opts MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t4/model_0014999.pth DISCOVER_UNKNOWN True
+  # CUDA_VISIBLE_DEVICES=${GPUS} python discover_unknown.py \
+  #                               --config-file configs/${BENCHMARK}/t4_ft.yaml \
+  #                               --input-txt datasets/ImageSets/Main/${BENCHMARK}/t4_ft.txt \
+  #                               --task ${BENCHMARK}/t4_ft \
+  #                               --output output/${BASELINE}/${BENCHMARK}/t4/unknown_rois.json \
+  #                               --opts MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t4/model_0014999.pth DISCOVER_UNKNOWN True
 
   CUDA_VISIBLE_DEVICES=${GPUS} python train_net.py \
                                 --num-gpus ${NUM_GPUS} \
@@ -211,6 +210,5 @@ else
                                 --config-file configs/${BENCHMARK}/t4_ft.yaml \
                                 --resume \
                                 MODEL.WEIGHTS output/${BASELINE}/${BENCHMARK}/t4/model_0014999.pth \
-                                DISCOVER_STORE_PATH output/${BASELINE}/${BENCHMARK}/t4/unknown_rois.json \
                                 OUTPUT_DIR output/${BASELINE}/${BENCHMARK}/t4/
 fi
